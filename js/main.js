90e1a4e6f4a311d1208e98241d308d25fd3d1040
erfan <erfanajoglu098@gmail.com>
2023-10-25 17:35:38 +0330

    add super product

--- js/main.js
+++ js/main.js
@@ -1,4 +1,19 @@
 let slideIndex = 1;
+let remainingTime = 70000;
+
+function setTime(){
+    let h = Math.floor(remainingTime/3600)
+    let m = Math.floor((remainingTime%3600)/60)
+    let s = (remainingTime%3600) % 60
+    document.querySelector(".hours").innerHTML = h;
+    document.querySelector(".minutes").innerHTML = m;
+    document.querySelector(".seconds").innerHTML = s;
+}
+
+setInterval(()=>{
+    remainingTime -= 1;
+    setTime();
+},1000)
 function setSlide(input , index){
     slideIndex = index;
     let active = document.getElementById(`${input}`);
